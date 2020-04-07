33d002861b50d31696032862611e27958d1b550c
holowizor <marek.kwiecien@gmail.com>
2020-04-07 22:26:55 +0200

    Gesture handling

--- src/main/kotlin/com/devbuild/ddapp/Main.kt
+++ src/main/kotlin/com/devbuild/ddapp/Main.kt
@@ -1,27 +1,34 @@
 package com.devbuild.ddapp
 
+import com.devbuild.ddapp.gesture.GestureDetector
+import com.devbuild.ddapp.gesture.GestureType
 import com.devbuild.waveshare213v2.Waveshare213v2
-import com.pi4j.io.gpio.GpioFactory
 import com.pi4j.io.gpio.RaspiPin
-import com.pi4j.io.gpio.event.GpioPinListenerDigital
+import org.slf4j.LoggerFactory
 import java.io.File
 import javax.imageio.ImageIO
 
 @Volatile
 var keepRunning = true
+val logger = LoggerFactory.getLogger("DisplayApp")
 
 fun main() {
-
-    val btn = GpioFactory.getInstance().provisionDigitalInputPin(RaspiPin.GPIO_02, "BTN")
-    btn.addListener(GpioPinListenerDigital {
-        if (it.state.isHigh) {
+    logger.info("initializing click button gestures")
+    val gestureDetector = GestureDetector(RaspiPin.GPIO_02)
+    gestureDetector.addListener {
+        logger.info("got gesture event {}", it)
+        if (it.type == GestureType.N_CLICK && it.amount == 1) {
             Display.display()
+        } else if (it.type == GestureType.N_CLICK && it.amount == 2) {
+            Waveshare213v2.clear(0xFFu)
         }
-    })
+    }
 
+    logger.info("adding shutdown hook")
     val mainThread = Thread.currentThread()
     Runtime.getRuntime().addShutdownHook(Thread {
-        fun  run() {
+        fun run() {
+            logger.info("shutting down...")
             keepRunning = false
             mainThread.join()
             Waveshare213v2.clear(0xFFu)
@@ -29,8 +36,10 @@ fun main() {
         }
     })
 
+    logger.info("preparing display")
     Waveshare213v2.fullUpdate()
     while (keepRunning) {
+        logger.info("waiting...")
         Thread.sleep(5000)
     }
 }
@@ -38,6 +47,7 @@ fun main() {
 object Display {
     @Volatile
     var displayBusy = false
+    var inc = 0
 
     fun display() {
         if (displayBusy) return
@@ -47,7 +57,7 @@ object Display {
         Waveshare213v2.printImage(image)
 
         // for testing
-        ImageIO.write(image, "jpg", File("test.jpg"));
+        ImageIO.write(image, "jpg", File("test-${inc++}.jpg"));
 
         Thread.sleep(5000)
         displayBusy = false
---
+++ src/main/kotlin/com/devbuild/ddapp/gesture/GestureDetector.kt
@@ -0,0 +1,87 @@
+package com.devbuild.ddapp.gesture
+
+import com.pi4j.io.gpio.GpioFactory
+import com.pi4j.io.gpio.Pin
+import com.pi4j.io.gpio.PinState
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent
+import com.pi4j.io.gpio.event.GpioPinListenerDigital
+import org.slf4j.LoggerFactory
+import java.util.concurrent.Executors
+
+val logger = LoggerFactory.getLogger("GestureDetector")
+
+enum class GestureType {
+    N_CLICK, LONG_PRESS
+}
+
+data class GestureEvent(val type: GestureType, val amount: Int)
+
+//@FunctionalInterface
+//interface GestureListener {
+//    fun onGesture(gestureEvent: GestureEvent)
+//}
+
+class GestureDetector(pin: Pin) : GpioPinListenerDigital {
+    init {
+        GpioFactory
+            .getInstance()
+            .provisionDigitalInputPin(pin, "BTN")
+            .addListener(this)
+    }
+
+    private val listeners = ArrayList<(GestureEvent) -> Unit>()
+    fun addListener(listener: (GestureEvent) -> Unit) = listeners.add(listener)
+
+    @Volatile private var t0 = 0L
+    @Volatile private var t1 = 0L
+    @Volatile private var clickCount = 0
+    private val executor = Executors.newCachedThreadPool()
+    @Volatile private var lastClickCountThread: ClickCountRunnable? = null
+
+    override fun handleGpioPinDigitalStateChangeEvent(event: GpioPinDigitalStateChangeEvent?) {
+        if (event?.state == PinState.HIGH) {
+            logger.info("state high")
+            lastClickCountThread?.send = false
+            t0 = System.currentTimeMillis();
+        } else if (event?.state == PinState.LOW) {
+            logger.info("state low")
+            t1 = System.currentTimeMillis()
+            if (t1 - t0 < 500) {
+                clickCount++
+                lastClickCountThread = ClickCountRunnable(this)
+                executor.submit(lastClickCountThread)
+            } else {
+                sendGestureEvent(GestureType.LONG_PRESS, ((t1 - t0) / 1000L).toInt())
+                reset()
+            }
+        }
+    }
+
+    private class ClickCountRunnable(val detector: GestureDetector) : Runnable {
+        @Volatile
+        var send = true
+
+        override fun run() {
+            Thread.sleep(500L)
+            if (send) {
+                val count = detector.clickCount
+                detector.reset()
+                // reset first as event handling can take some time - run it in separate thread?
+                detector.sendGestureEvent(GestureType.N_CLICK, count)
+            }
+        }
+    }
+
+    private fun sendGestureEvent(type: GestureType, amount: Int) =
+        listeners.forEach {
+            logger.info("sending $type, $amount event")
+            it.invoke(GestureEvent(type, amount))
+        }
+
+    private fun reset() {
+        logger.info("resetting counters")
+        t0 = 0L
+        t1 = 0L
+        clickCount = 0
+    }
+}
